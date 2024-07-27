30b580c1e16f05564f67182cb4d917ae760fbd65
almdashif <ashifalmohammed@gmail.com>
2024-07-27 21:18:54 +0530

    var in css added

--- src/Loginpage/LoginPage.jsx
+++ src/Loginpage/LoginPage.jsx
@@ -74,6 +74,3 @@ const LoginPage = () => {
 }
 
 export default LoginPage
-
-// https://images.unsplash.com/photo-1524499982521-1ffd58dd89ea?q=80&w=1871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
-// https://images.unsplash.com/photo-1618498082410-b4aa22193b38?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
